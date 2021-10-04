9ebfafb55fbcab5bc8a82de57663f13857602dd0
KentoHardRok <tom.wilent@gmail.com>
2021-10-04 14:02:18 -0400

    added working pull ip info

--- wg-mini/gather_facts.py
+++ wg-mini/gather_facts.py
@@ -1,4 +1,7 @@
 import netifaces as ni
 ##ni.ifaddresses('eth0')
+# netifaces.ifaddresses('docker0')[2][0]['addr'] example i found to pull ip information
+# This is for if you're behind a NAT and
+# want the connection to be kept alive.
 ip = ni.interfaces() ##ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
 print(ip)
