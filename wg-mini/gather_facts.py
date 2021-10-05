c683f4249d24858d048d5e18f7dc9699bcebfefa
KentoHardRok <tom.wilent@gmail.com>
2021-10-05 14:14:05 -0400

    mroe focused containers

--- wg-mini/gather_facts.py
+++ wg-mini/gather_facts.py
@@ -1,5 +1,17 @@
 import netifaces as ni
-##ni.ifaddresses('eth0')
-# netifaces.ifaddresses('docker0')[2][0]['addr'] example i found to pull ip information
-ip = ni.interfaces() ##ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
-print(ip)
+import jinja2 import Template as tl
+
+with open("client_base") as file_:
+    template = tl(file_.read())
+
+jinja_var={}
+
+jinja_var['SERV_EXT_IP'] = ni.ifaddresses('eth0')[2][0]['addr'] 
+
+with open ("serv.priv", "r") as private:
+    jinja_var['SERV_PRIV']=private.readlines()
+
+with open ("serv.pub", "r") as public:
+    jinja_var['SERV_PUB']=public.readlines()
+
+print(template.render(jinja_var))
