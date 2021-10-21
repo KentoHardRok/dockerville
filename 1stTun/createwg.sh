61e6fc5309f621c2ac3fe6437af6cf789a57c215
KentoHardRok <tom.wilent@gmail.com>
2021-10-21 15:08:20 -0400

    fixed sed

--- 1stTun/createwg.sh
+++ 1stTun/createwg.sh
@@ -2,14 +2,14 @@
 
 set -euo pipefail
 
+wg genkey | tee key.priv | wg pubkey > key.pub
 source vars
 
 cd $WG_DIR
 
 create_server() {
     umask 077
-    wg genkey | tee key.priv | wg pubkey > key.pub
-    sed -i s/"<SERVER PRIVATE KEY>"/$SERVER_PRIV/g wg0.conf    
+    sed -i "s|<SERVER PRIVATE KEY>|$SERVER_PRIV|g" wg0.conf    
 }
 
 create_client() {
@@ -18,12 +18,12 @@ create_client() {
     PEERCONF="$NAME.conf"
     wg genkey | tee $NAME.priv | wg pubkey > $NAME.pub
     cat peer_setup.conf >> wg0.conf
-    sed -i s/"<CLIENT PUBLIC KEY>"/"$(<$NAME.pub)"/g wg0.conf    
-    sed -i s/"<CLIENT PRIV IP>"/$(expr 1 + $1)/g wg0.conf    
+    sed -i "s|<CLIENT PUBLIC KEY>|$(cat $NAME.pub)|g" wg0.conf    
+    sed -i "s|<CLIENT PRIV IP>|$(expr 1 + $1)|g" wg0.conf    
     cat wgpeer.conf >> $PEERCONF
-    sed -i s/"<EXT IP>"/$EXT_IP/g $PEERCONF    
-    sed -i s/"<SERVER PUBLIC KEY>"/$SERVER_PUB/g $PEERCONF
-    sed -i s/"<CLIENT PRIVATE KEY>"/"$(<$NAME.priv)"/g $PEERCONF    
+    sed -i "s|<EXT IP>|$EXT_IP|g" $PEERCONF    
+    sed -i "s|<SERVER PUBLIC KEY>|$SERVER_PUB|g" $PEERCONF
+    sed -i "s|<CLIENT PRIVATE KEY>|$(cat $NAME.priv)|g" $PEERCONF    
 
 }
 create_server
