081ef83fbd5a0fd4de5e183ddf915a9bafc33b14
KentoHardRok <tom.wilent@gmail.com>
2021-10-21 12:42:16 -0400

    update scripts

--- 1stTun/createwg.sh
+++ 1stTun/createwg.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 set -euo pipefail
 
@@ -16,17 +16,20 @@ create_server() {
 create_client() {
     local NAME="client${1:0}"
     umask 077
-    peerconf=
+    PEERCONF="$NAME.conf"
     wg genkey | tee $NAME.priv | wg pubkey > $NAME.pub
     cat peer_setup.conf >> wg0.conf
     sed -i s/"<CLIENT PUBLIC KEY>"/$CLIENT_PUB/g wg0.conf    
     sed -i s/"<CLIENT PRIV IP>"/$(expr 1 + $1)/g wg0.conf    
-    sed -i s/"<EXT IP>"/$EXT_IP/g wgpeer0.conf    
+    cat wgpeer.conf >> $PEERCONF
+    sed -i s/"<EXT IP>"/$EXT_IP/g $PEERCONF    
+    sed -i s/"<SERVER PUBLIC KEY>"/$SERVER_PUB/g $PEERCONF
+    sed -i s/"<CLIENT PRIVATE KEY>"/$CLIENT_PRIV/g $PEERCONF    
 
 }
 create_server
 for ((i=0; i <= $NUM_CLIENTS; i++)); do
-    create_client $i;
+    create_client $i
     done
 
 
