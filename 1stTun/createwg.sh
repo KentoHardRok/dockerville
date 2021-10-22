03d84744872d7a257d20393d6041a5b46ff99666
KentoHardRok <tom.wilent@gmail.com>
2021-10-22 07:37:55 -0400

    added ext.ip port interpretation

--- 1stTun/createwg.sh
+++ 1stTun/createwg.sh
@@ -2,21 +2,19 @@
 
 set -euo pipefail
 
-cd /etc/wireguard
-wg genkey | tee key.priv | wg pubkey > key.pub
-source vars
-
-
 
 create_server() {
     umask 077
+    cd /etc/wireguard
+    wg genkey | tee key.priv | wg pubkey > key.pub
+    source vars
     sed -i "s|<SERVER PRIVATE KEY>|$SERVER_PRIV|g" wg0.conf    
+    sed -i "s|<PORT>|$WG_PORT|g" wg0.conf    
 }
 
 
 create_client() {
     local NAME="client${1:0}"
-    umask 077
     PEERCONF="$NAME.conf"
     wg genkey | tee $NAME.priv | wg pubkey > $NAME.pub
     cat peer_setup.conf >> wg0.conf
@@ -25,6 +23,7 @@ create_client() {
     cat wgpeer.conf >> $PEERCONF
     sed -i "s|<CLIENT PRIV IP>|$(expr 1 + $1)|g" $PEERCONF    
     sed -i "s|<EXT IP>|$EXT_IP|g" $PEERCONF    
+    sed -i "s|<PORT>|$WG_PORT|g" $PEERCONF
     sed -i "s|<SERVER PUBLIC KEY>|$SERVER_PUB|g" $PEERCONF
     sed -i "s|<CLIENT PRIVATE KEY>|$(cat $NAME.priv)|g" $PEERCONF    
 
