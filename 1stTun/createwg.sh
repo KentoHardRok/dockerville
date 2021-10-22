b95a70381265069c1323afa91819bd7c40996178
KentoHardRok <tom.wilent@gmail.com>
2021-10-22 12:11:51 -0400

    built an image!

--- 1stTun/createwg.sh
+++ 1stTun/createwg.sh
@@ -5,28 +5,33 @@ set -euo pipefail
 
 create_server() {
     umask 077
+    WG_DIR=/etc/wireguard
+    WG_PORT=51820
+    NUM_CLIENTS=0
+    EXT_IP=172.16.1.100
     cd /etc/wireguard
     wg genkey | tee key.priv | wg pubkey > key.pub
-    SERVER_PRIV=$(cat $WG_DIR/key.priv)
-    SERVER_PUB=$(cat $WG_DIR/key.pub)
-    sed -ei "s|<SERVER PRIVATE KEY>|$SERVER_PRIV|g" \    
-        -ei "s|<PORT>|$WG_PORT|g" wg0.conf    
+    SERVER_PRIV=$(cat /etc/wireguard/key.priv)
+    SERVER_PUB=$(cat /etc/wireguard/key.pub)
+    sed -i "s|<SERVER PRIVATE KEY>|$SERVER_PRIV|g" wg0.conf    
+    sed -i "s|<PORT>|$WG_PORT|g" wg0.conf    
 }
 
 
 create_client() {
-    local NAME="client${1:0}"
+    NAME="client${1:0}"
+    touch /etc/wireguard/client/$NAME.conf
     PEERCONF="/etc/wireguard/client/$NAME.conf"
     wg genkey | tee $NAME.priv | wg pubkey > $NAME.pub
     cat peer_setup.conf >> wg0.conf
-    sed -e "s|<CLIENT PUBLIC KEY>|$(cat $NAME.pub)|g" \    
-        -e "s|<CLIENT PRIV IP>|$(expr 1 + $1)|g" -i wg0.conf    
+    sed -i "s|<CLIENT PUBLIC KEY>|$(cat $NAME.pub)|g" $PEERCONF    
+    sed -i "s|<CLIENT PRIV IP>|$(expr 1 + $1)|g" -i wg0.conf    
     cat wgpeer.conf >> $PEERCONF
-    sed -e "s|<CLIENT PRIV IP>|$(expr 1 + $1)|g" \    
-        -e "s|<EXT IP>|$EXT_IP|g" \    
-        -e "s|<PORT>|$WG_PORT|g" \
-        -e "s|<SERVER PUBLIC KEY>|$SERVER_PUB|g" \
-        -e "s|<CLIENT PRIVATE KEY>|$(cat $NAME.priv)|g" -i $PEERCONF    
+    sed -i "s|<CLIENT PRIV IP>|$(expr 1 + $1)|g" $PEERCONF    
+    sed -i "s|<EXT IP>|$EXT_IP|g" $PEERCONF    
+    sed -i "s|<PORT>|$WG_PORT|g" $PEERCONF
+    sed -i "s|<SERVER PUBLIC KEY>|$SERVER_PUB|g" $PEERCONF
+    sed -i "s|<CLIENT PRIVATE KEY>|$(cat $NAME.priv)|g" -i $PEERCONF    
 
 }
 
